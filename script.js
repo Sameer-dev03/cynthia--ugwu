b61c4db1d2f574e94bcb02a4364551b788c7486c
sameer <sameeransari@478787@gmail.com>
2026-04-10 04:13:38 -0700

    completed

--- script.js
+++ script.js
@@ -1,4 +1,47 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+  });
+
+
+function mousecurser(){
+    const mouse =  document.querySelector("#mousecursor");
+    const main = document.querySelector("#main")
+
+   main.addEventListener("mousemove",  function(det ){
+
+        mouse.style.left = `${det.clientX}px`;
+    mouse.style.top = `${det.clientY}px`;
 
 
 
+    })
+}
 
+mousecurser()
+
+
+function servicemove() {
+    const elem = document.querySelectorAll(".elem");
+
+    elem.forEach(function(service) {
+        const img = service.querySelector("img");
+
+        service.addEventListener("mousemove", function(det) {
+            const rect = service.getBoundingClientRect();
+
+            const x = det.clientX - rect.left;
+            const y = det.clientY - rect.top;
+
+            img.style.display = "block";
+            img.style.left = `${x}px`;
+            img.style.top = `${y}px`;
+        });
+
+        service.addEventListener("mouseleave", function() {
+            img.style.display = "none";
+        });
+    });
+}
+
+servicemove();  
